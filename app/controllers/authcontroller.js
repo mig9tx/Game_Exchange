38fd61584c0a5abc6c9f8e0dd08796e2880e0811
mig9tx <migue.trujillo@utexas.edu>
2018-12-11 14:24:32 -0600

    reverted back authController

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -3,15 +3,40 @@ const db = require("../models");
 
 exports = module.exports = {};
 
-exports.signup = function(req, res) {
+exports.signup = function (req, res) {
     res.render("signup");
 };
 
-exports.signin = function(req, res) {
+exports.signin = function (req, res) {
     res.render("signin");
 };
 
-exports.dashboard = function(req, res) {
+// exports.dashboard = function (req, res) {
+//     const query = {};
+//     if (req.query.user_id) {
+//         query.UserId = req.query.user_id;
+//     }
+//     //Here we add an "include" property to our option in our findAll query
+//     //We set the value to an array of the models we want to include in a left outer join
+//     //In this case, just db.User
+//     db.Game.findAll({
+//         where: {
+//             UserId: req.user.id
+//         }
+//         // include: [db.User]
+//     }).then(function (dbGame) {
+//         // console.log(dbGame);
+//         // console.log(req.user);
+//         console.log('*******************************************************************************')
+//         console.log(dbGame);
+//         res.render("dashboard", {
+//             dbGames: dbGame,
+//             user: req.user
+//         }); //JSON.stringify
+//     });
+// };
+exports.dashboard = function (req, res) {
+
     const query = {};
     if (req.query.user_id) {
         query.UserId = req.query.user_id;
@@ -24,28 +49,43 @@ exports.dashboard = function(req, res) {
             UserId: req.user.id
         }
         // include: [db.User]
-    }).then(function(dbGame) {
+    }).then(function (dbGame) {
         // console.log(dbGame);
         // console.log(req.user);
-        console.log(dbGame);
+        // console.log('*******************************************************************************')
+        // console.log(dbGame);
         res.render("dashboard", {
             dbGames: dbGame,
             user: req.user
         }); //JSON.stringify
     });
+
+    // console.log(url);
+    // axios.get(url).then((response) =>{{
+    //     console.log(response);
+    //     // const {
+    //     //     title,
+    //     //     console,
+    //     //     image,
+    //     //     gsPriceBuy,
+    //     //     gsPriceSell,
+    //     //     userSellPrice
+    //     // } = response.data;
+    //     // data.gameData = {
+    //     //     title,
+    //     //     console, //gamePrice: gamePrice
+    //     //     image,
+    //     //     gsPriceBuy,
+    //     //     gsPriceSell,
+    //     //     userSellPrice
+    // });
+
+    // console.log(data.gameData);
 };
 
-exports.postGame = function(req, res) {
-    const url =
-        "https://www.pricecharting.com/api/products?" +
-        process.env.PRICE_KEY +
-        "&q=" +
-        req.body.gameTitle +
-        " " +
-        req.body.gameConsole;
-
+exports.postGame = function (req, res) {
+    const url = "https://www.pricecharting.com/api/products?" + process.env.PRICE_KEY + "&q=" + req.body.gameTitle + " " + req.body.gameConsole
     console.log(url);
-
     axios.get(url).then((response) => {
         res.json(response.data);
     });
@@ -57,13 +97,6 @@ exports.postGame = function(req, res) {
     // res.render("dashboard", data);
 };
 
-exports.logout = function(req, res) {
-    req.session.destroy(function() {
-        req.logout();
-        res.redirect("/");
-    });
-};
-
 exports.searchgame = (req, res) => {
     const query = {};
     if (req.query.user_id) {
@@ -73,19 +106,23 @@ exports.searchgame = (req, res) => {
     //We set the value to an array of the models we want to include in a left outer join
     //In this case, just db.User
     db.Game.findAll({
-        where: {
-            UserId: {
-                $notIn: [req.user.id]
-            }
-        }
+        where: { UserId: { $notIn: [req.user.id] } }
         // include: [db.User]
-    }).then(function(dbGame) {
+    }).then(function (dbGame) {
         // console.log(dbGame);
         // console.log(req.user);
+        console.log('*******************************************************************************')
         console.log(dbGame);
         res.render("searchgame", {
             dbGames: dbGame,
             user: req.user
         }); //JSON.stringify
+    });
+};
+
+exports.logout = function (req, res) {
+    req.session.destroy(function () {
+        req.logout();
+        res.redirect("/");
     });
 };
