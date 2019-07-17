5ca9d7ef3091ab7a60ed3e55d6e7e7efab62e2db
mig9tx <miguel.trujillo@utexas.edu>
2019-07-16 22:49:32 -0500

    updated vulenerable dependencies

--- app/models/index.js
+++ app/models/index.js
@@ -8,11 +8,14 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+
+
+
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config, { insecureAuth: true });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config, { insecureAuth: true });
 }
 
 fs
